93a1b18ddc080564022f5ca3745b93f6ac29036f
Misato Fujihara <fujiharaionn@gmail.com>
2017-08-24 10:27:27 +0900

    Inputくらすの作成

--- game.rb
+++ game.rb
@@ -1,2 +1,17 @@
 # encoding: UTF-8
+# 入出力関数の作成
+class Input
+  def initialize(x, y)
+    @x=x
+    @y=y
+  end
+  def check_position
+    if (table[@y][@x])
+        return true
+    else
+        return false
+    end
+  end
+end
+$table = Array.new(3).map{Array.new(3,0)}
 puts("○×ゲームを始めます")
