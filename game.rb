d2ed6e7eff823e5012049cc750063fac984c06c1
Misato Fujihara <fujiharaionn@gmail.com>
2017-08-24 18:06:18 +0900

    インスタンス変数の使用を控えてエラー解除

--- game.rb
+++ game.rb
@@ -1,11 +1,6 @@
 # encoding: UTF-8
 # 入出力関数の作成
 
-# tableの添え字を場所に振られた数字に変える
-def index_number(x,y)
-  return x+1+(y)*3;
-end
-
 # maruの状態(trueかfalseか)をtableの状態にして返す
 # ○がおかれていたら表の状態は1,×がおかれていたら表の状態は2
 def maru_state
@@ -24,68 +19,83 @@ class ClearCheck
   end
   # 斜め方向にそろっているか確認
   def naname
+    i,j=@i,@j
     clear=0
-    while($table[@j][@i]==maru_state()) do
+    while($table[j][i]==maru_state()) do
       clear+=1
       return 1 if(clear>2)
-      @i+=1
-      @j+=1
+      i+=1
+      j+=1
     end
     return 0
   end
   # 縦方向にそろっているか確認
   def tate
+    i,j=@i,@j
     clear=0
-    while($table[@j][@i]==maru_state()) do
+    while($table[j][i]==maru_state()) do
       clear+=1
       return 1 if(clear>2)
-      @j+=1
+      j+=1
     end
     return 0
   end
   # 横方向にそろっているか確認
   def yoko
+    i,j=@i,@j
     clear=0
-    while($table[@j][@i]==maru_state()) do
+    while($table[j][i]-maru_state()==0) do
       clear+=1
       return 1 if(clear>2)
-      @i+=1
+      i+=1
     end
     return 0
   end
 end
 
- 
+ # 3つそろっていたら１,そうでなければ0を返す関数
 def callClear
-  
+  # table[0][0]は縦横斜め方向にそろっているかを調べる
   check=ClearCheck.new(0,0)
   if(check.naname()==1||check.yoko()==1||check.tate()==1)
     return 1
   end
+  
   check2=ClearCheck.new(1,0)
   check3=ClearCheck.new(2,0)
   check4=ClearCheck.new(0,1)
   check5=ClearCheck.new(0,2)
+  # table[1][0],table[2][0]は横方向にそろっているか調べる
   if(check2.yoko()==1)
     return 1
   end
   if(check3.yoko()==1)
     return 1
   end
+  # table[0][1],tanle[0][2]は縦方向にそろっているか調べる
   if(check4.tate()==1)
     return 1
   end
   if(check5.tate()==1)
     return 1
   end
+  # どこもそろっていなかったら0を返す
   return 0
 end
     
-
+# 出力される表の位置番号をtableの添え字に変換
 def number_index(num)
-  x=(num-1)%3
-  y=(num-1)/3
+  x=(num)%3-1
+  y=(num-(x+1))/3
+  if(num%3==0)
+    y-=1
+  end
   return x,y
+end
+
+# tableの添え字を出力される表の位置番号に変換
+def index_number(x,y)
+  return x+1+y*3;
 end
 
 class Set_position
@@ -131,7 +141,7 @@ def call_output
   i=0
   print("\n")
   $table.each do |elm1, elm2, elm3|
-    #print("   |   |   |\n")
+    
     output(i,elm1, elm2, elm3) 
     i+=1
   end
@@ -140,10 +150,11 @@ end
 def call_Set(num)
   x,y=number_index(num)
   pos=Set_position.new(x,y)
-  if (pos.check_position==false||num>9||num<1)
+  if (num>9||num<1||pos.check_position==false)
     puts("もう一度入力してください")
     input()
   else
+    p x,y
     $table[y][x]=maru_state()
   end
 end
