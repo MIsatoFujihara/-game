a2318cdd319dc4e2673a4ae8d12133bb2e743157
Misato Fujihara <fujiharaionn@gmail.com>
2017-08-25 14:13:40 +0900

    unlessの使用

--- clear_check.rb
+++ clear_check.rb
@@ -26,7 +26,7 @@ class ClearCheck
     # 表中の確認したい左下斜め方向に並んだ要素を1行の配列として格納
     check_table = [@table[j][i], @table[j - 1][i + 1], @table[j - 2][i + 2]]
     check_table.each do |point|
-      break if(point != maru_state(@maru))
+      break unless(point == maru_state(@maru))
       clear += 1
       #3つ以上連続してそろっていたらbreak
       return 1 if(clear >= 3)
@@ -41,7 +41,7 @@ class ClearCheck
     # 表中の確認したい列のみを1行の配列としてcheck_tableに格納
     check_table = @table[i]
     check_table.each do |point|
-      break if(point != maru_state(@maru))
+      break unless(point == maru_state(@maru))
       clear += 1
       #3つ以上連続してそろっていたらbreak
       return 1 if(clear >= 3)
@@ -55,7 +55,7 @@ class ClearCheck
     # 表中の確認したい行のみを1行の配列としてcheck_tableに格納
     check_table = @table[j]
     check_table.each do |point|
-        break if(point != maru_state(@maru))
+        break unless(point == maru_state(@maru))
         clear += 1
         #3つ以上連続してそろっていたらbreak
         return 1 if(clear >= 3)
